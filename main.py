9629f10d862e6115c8a0d2d847c3e0417775b1e0
Kevin <kye2@vols.utk.edu>
2018-09-29 05:21:30 -0400

    Speedup
    Use video port arg in stream to get like ludicrous frame rate

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import picamera
 import cv2
 import time
 
+IMG_WIDTH = 240
+IMG_HEIGHT = 240
+
 def testCamera():
     camera = picamera.PiCamera()
-    camera.resolution = (640,640)
+    camera.resolution = (240,240)
     camera.start_recording("test.h264")
     camera.wait_recording(5)
     camera.stop_recording()
@@ -13,20 +16,22 @@ def testCamera():
 if __name__ == "__main__":
     #argparse?
 
+
     with picamera.PiCamera() as camera:
-        camera.start_preview()
+        #camera.start_preview()
         #camera finishes warming up
 
-        camera.resolution = (640,480)
+        camera.resolution = (IMG_WIDTH,IMG_HEIGHT)
         camera.framerate = 32
         time.sleep(2)
-        with picamera.array.PiRGBArray(camera, size=(640,480)) as stream:
-            for frame in camera.capture_continuous(stream, format='bgr'):
+        with picamera.array.PiRGBArray(camera, size=(IMG_WIDTH,IMG_HEIGHT)) as stream:
+            for frame in camera.capture_continuous(stream, format='bgr', use_video_port=True):
                 image = frame.array
                 cv2.imshow("Frame", image)
+                print "frame read"
 
-                #key = cv2.waitKey(1) & 0xFF
+                key = cv2.waitKey(1) & 0xFF
 
                 stream.truncate(0)
-                #if key == ord('q'):
-                    #break
+                if key == ord('q'):
+                    break
