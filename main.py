82cbebc6ce8bf4a7c1f06e7a75b34e09fcf1106f
Kevin <kye2@vols.utk.edu>
2018-09-29 04:24:00 -0400

    reading images into opencv array

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import picamera.array
 import picamera
+import cv2
+import time
 
-if __name__ == "__main__":
-    #argparse?
+def testCamera():
     camera = picamera.PiCamera()
     camera.resolution = (640,640)
     camera.start_recording("test.h264")
     camera.wait_recording(5)
     camera.stop_recording()
 
+if __name__ == "__main__":
+    #argparse?
+
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        #camera finishes warming up
+
+        camera.resolution = (640,480)
+        camera.framerate = 32
+        time.sleep(2)
+        with picamera.array.PiRGBArray(camera, size=(640,480)) as stream:
+            for frame in camera.capture_continuous(stream, format='bgr'):
+                image = frame.array
+                cv2.imshow("Frame", image)
+
+                #key = cv2.waitKey(1) & 0xFF
+
+                stream.truncate(0)
+                #if key == ord('q'):
+                    #break
