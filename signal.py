edb30dc317d527a0e58b40f75c9d56389f9a24ee
Kevin Ye <kye2@vols.utk.edu>
2018-09-30 05:17:21 -0400

    Init Signal Framework
    Move the turret with dead reckoning

--- signal.py
+++ signal.py
@@ -1,10 +1,44 @@
 import serial
+import RPi.GPIO as GPIO
 
 ser=serial.Serial('/dev/ttyACM0', 9600)
 
 ser.flushInput()
+GPIO.cleanup()
+GPIO.setup(channel, GPIO)
+GPIO.setmode(GPIO.BOARD)
 
-while 1:
-	ser.write('A')
-	var = ser.read()
-	print(var)
+def testSerial():
+	while 1:
+		ser.write('A')
+		var = ser.read()
+		print(var)
+
+def pinHigh(pin):
+	GPIO.output(pin, GPIO.HIGH)
+
+def pinHigh(pin):
+	GPIO.output(pin, GPIO.LOW)
+
+def rotateYaw(degrees):
+	#40 = high 
+	#38 = low
+	#36 = high
+	#all low to turn off
+
+	#constant to convert degrees to seconds
+	deg2sec = 0.1;
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
+	
+
+def rotatePitch(degrees):
+	#8 = high
+	#10 = low
+	#12 = high
+	#all low to turn off
+
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
+	GPIO.setup(40, GPIO.OUT, intiial=GPIO.LOW)
